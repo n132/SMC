1fd53e405e1fe25df37643a6c5fe11be684f79b7
n132 <xm2146@nyu.edu>
2021-12-08 16:50:40 -0500

    Update GMW.py

---
+++ Primitives/GMW.py
@@ -0,0 +1,12 @@
+# n132
+# How to play any mental game, or a completeness theorem for protocols with honest majority
+from OT4 import *
+
+
+
+def main():
+    pass
+def n132():#tester
+
+if __name__ == '__main__':
+    main()
