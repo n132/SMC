58957190b5468fc5ad8e29f70472acff09ec9878
n132 <xm2146@nyu.edu>
2021-12-08 13:51:54 -0500

    OT2 Reconstruct

--- Primitives/Alice.py
+++ Primitives/Alice.py
@@ -1,32 +1,7 @@
-import socket
+from OT2 import *
 PORT = 6999
 from random import randint
-from utils import *
-def otEncrypt(g,p,pk,s):
-    r=randint(1,p-1)
-    tmp= n2b(pow(g,r,p))
-    tmp2 = bytes_to_long(hash( n2b( pow(pk,r,p) ) )) ^ bytes_to_long(s)
-    return tmp+b'-'+n2b(tmp2)
-def OT_Alice(message,client):
-    client.send(b"Hi")
-    data= client.recv(1024)
-    p, g=data.split(b"-")
-    p = int(p)
-    g = int(g)
-    c = randint(1,p-1)
-    client.send(n2b(c))
-    data = client.recv(1024)
-    pks = data.split(b"-")
-    pks[0] = int(pks[0])
-    pks[1] = int(pks[1])
-    assert(c== (pks[0] * pks[1])% p)
-    sec = message
-    data=b""
-    data+= otEncrypt(g,p,pks[0],sec[0])
-    data+=b'-'
-    data+= otEncrypt(g,p,pks[1],sec[1])
-    client.send(data)
-    client.close()
+
 def Alice():
     s = socket.socket()
     host = socket.gethostname()
@@ -34,8 +9,7 @@ def Alice():
     s.listen(5)
     while True:
         client,addr = s.accept()
-        OT_Alice([b"This is secret 0",b"This is secret 1"],client)
+        OT_Sender([b"This is secret 0",b"This is secret 1"],client)
     
-
 if __name__ == '__main__':
     Alice()
--- Primitives/Bob.py
+++ Primitives/Bob.py
@@ -1,40 +1,10 @@
-import socket
-from utils import *
+from OT2 import *
 PORT = 6999
-BigPrime = 4776913109852041418248056622882488319
-def OT_Bob(cmd,s):
-    m= s.recv(2)
-    if(m==b'Hi'):
-        GC = CyclicGroup(BigPrime)
-        g = GC.generator
-        data = n2b(BigPrime)+b"-"+n2b(g)
-        s.send(data)
-        c = int(s.recv(1024))
-        k = GC.rand_int()
-        pks=[0,0]
-        b = cmd
-        pks[b] = GC.pow(g,k)
-        pks[b-1] = GC.div(c,pks[b])
-        
-        s.send(pack(pks))
-        data = s.recv(1024).split(b"-")
-        b = b*2
-        u = data[b]
-        v = data[b+1]
-        u = int(u)
-        v = int(v)
-        res=long_to_bytes( bytes_to_long(hash(n2b(GC.pow(u,k))))  ^ v )
-        s.close()
-        return int(res[-1:])
-    else:
-        s.close()
-        return 2
-
 def Bob(cmd):
     s = socket.socket()
     host = socket.gethostname()
     s.connect((host, PORT))
-    return OT_Bob(cmd,s)
+    return OT_Receiver(cmd,s)
 
 if __name__ == "__main__":
     ct = 0 
