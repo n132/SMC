c2b5fdf9102b5761114ce7a32f624aaaa8963abe
LockonS483 <rumblie.gg@gmail.com>
2021-12-11 21:33:25 -0500

    added arbitrary input to cgarbl2

---
+++ imp_hou/ottesta2.py
@@ -0,0 +1,108 @@
+from cgarbl2 import *
+import gcot
+import pickle
+from random import randint
+from utils import *
+
+""" # client receives secrets from server
+def ottest1():
+    s = connect()
+    res = gcot.OT_Receiver(0, s)
+    print(res)
+
+# cgarbl socket test
+def cgarbtesta():
+    with open("./bigEquals.json") as f:
+        data = f.read()
+    
+    cinfo = json.loads(data)
+    lb = circuitLabels(cinfo)
+    gc, rmap = garbleCircuit(cinfo, lb)
+
+    araw = [randint(0,1) for i in range(2)]
+    ains = [lb[0][araw[0]], lb[1][araw[1]]] # grab keys for alice inputs
+    gcinfo = {
+        "gc": gc,
+        "cinfo": cinfo,
+        "inputs": ains,
+        "rmap": rmap,
+        "raw_ins": araw
+    }
+    #print(gcinfo)
+
+    # need to serialize gc and send to bob
+    s = connect()
+    s.sendall(pickle.dumps(gcinfo)) # send all info to bob
+
+    OTDone = False
+    innum = 0
+    b_secrets = {0: lb[2], 1: lb[3]}
+    print("b_secrets", b_secrets)
+    msg = s.recv(256)
+    while(not OTDone):
+        print("secrets to send:",b_secrets[innum])
+        otres = gcot.OT_Sender(b_secrets[innum], s)
+        print("ot ended with", otres)
+        if(otres == 1):
+            innum+=1
+        bres = s.recv(256)
+        if(bres == b'ins-done'):
+            OTDone = True
+    
+    msg = s.recv(4)
+    result = int.from_bytes(msg, 'big')
+    print("RESULT:", result) """
+
+def multiintest():
+    # cgarbl socket test
+    with open("./bigEquals2.json") as f:
+        data = f.read()
+    
+    cinfo = json.loads(data)
+    lb = circuitLabels(cinfo)
+    gc, rmap = garbleCircuit(cinfo, lb)
+
+    araw = [randint(0,1) for i in range(len(cinfo["a_inputs"]))]
+    ains = ['' for i in range(len(araw))]
+    for i in range(len(araw)): # grab keys for alice inputs
+        ains[i] = lb[cinfo["a_inputs"][i]][araw[i]]
+    gcinfo = {
+        "gc": gc,
+        "cinfo": cinfo,
+        "inputs": ains,
+        "rmap": rmap,
+        "raw_ins": araw # nonessential info for correctness testing ONLY
+    }
+    #print(gcinfo)
+
+    # need to serialize gc and send to bob
+    s = connect()
+    s.sendall(pickle.dumps(gcinfo)) # send all info to bob
+
+    OTDone = False
+    innum = 0
+    b_secrets = {}
+    for cbin in cinfo["b_inputs"]:
+        b_secrets[innum] = lb[cinfo["b_inputs"][innum]]
+        innum+=1
+    innum = 0
+    print("b_secrets", b_secrets)
+    msg = s.recv(256)
+    while(not OTDone):
+        print("secrets to send:",b_secrets[innum])
+        otres = gcot.OT_Sender(b_secrets[innum], s)
+        print("ot ended with", otres)
+        if(otres == 1):
+            innum+=1
+        bres = s.recv(256)
+        if(bres == b'ins-done'):
+            OTDone = True
+    
+    msg = s.recv(4)
+    result = int.from_bytes(msg, 'big')
+    print("RESULT:", result)
+
+if(__name__ == "__main__"):
+    # ottest1()
+    for i in range(1000):
+        multiintest()
---
+++ imp_hou/ottestb2.py
@@ -0,0 +1,93 @@
+import cgarbl2
+import gcot
+import pickle
+from utils import *
+
+import time
+
+# server sends secrets to client
+def ottestb():
+    with listen() as s:
+        while(True):
+            conn, addr = s.accept()
+            with conn: 
+                print("connected by", addr)
+                tmp = gcot.OT_Sender([b'aaaaaaaaaa', b'bbbbbbbbbb'], conn)
+                conn.close()
+
+def cgarbltestb():
+    STARTFLAG = False
+    STARTTIME = 0.0
+    ENDTIME = 0.0
+
+    with listen() as s:
+        rounds = 0
+        correct = 0
+        while(rounds < 1000):
+            conn, addr = s.accept()
+            with conn:
+                if(not STARTFLAG):
+                    STARTTIME = time.time()
+                    STARTFLAG = True
+                
+                # receive stuff from alice 
+                a = conn.recv(256000)
+                """
+                {
+                    "gc": {}
+                    "cinfo": {}
+                    "inputs": [b'xxx', b'xxx']
+                    "rmap": {}
+                }
+                """
+                amsg = pickle.loads(a)
+                print("received:\n",amsg)
+
+                # generate b inputs and ask for OT
+                b_in_raw = [randint(0,1) for i in range(len(amsg["cinfo"]["b_inputs"]))]
+                b_ins = [''] * len(amsg["cinfo"]["b_inputs"])
+                conn.sendall(b'ready')
+                ci=0
+                while(b_ins[ci] == ""):
+                    print("attempting ot",ci)
+                    res = gcot.OT_Receiver(b_in_raw[ci], conn)
+                    if(not res == -1):
+                        b_ins[ci] = res
+                    print("received secret:", b_ins[ci])
+                    ci += 1
+                    if(ci < len(b_in_raw)):
+                        conn.sendall(b'continue')
+                    else:
+                        break
+
+                conn.sendall(b'ins-done')
+                print("b_ins", b_ins)
+
+                # build inputs and eval circuit
+                final_inputs = amsg["inputs"] + b_ins
+                gc = amsg["gc"]
+                cinfo = amsg["cinfo"]
+                rmap = amsg["rmap"]
+
+                eval_result = cgarbl2.evalCircuit(gc, final_inputs, cinfo, rmap)
+                print("RESULT:",eval_result)
+
+                conn.sendall(eval_result.to_bytes((eval_result.bit_length() + 7 ) // 8, 'big'))
+
+                # correctness check
+                a_raw_inputs = amsg["raw_ins"]
+                b_raw_inputs = b_in_raw
+                raw_inputs = a_raw_inputs + b_raw_inputs
+                real = (not (a_raw_inputs == b_raw_inputs))
+                print("correct:", (real==eval_result))
+                if(real == eval_result):
+                    correct += 1
+                rounds += 1
+        ENDTIME = time.time()
+        print("ratio:", correct/rounds)
+        print("Time Elapsed:", (ENDTIME - STARTTIME))
+
+if(__name__ == "__main__"):
+    #ottestb()
+    cgarbltestb()
+    #singletestb()
